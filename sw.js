b973f2b75f7d68807ec5d95666f4f470c575ce68
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-17 13:09:58 -0500

    76 cache con network fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const STATIC_CACHE = 'static-v1';
+//imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -12,7 +15,8 @@ const APP_SHELL = [
     'img/avatars/ironman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
-    'js/app.js'
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
@@ -53,4 +57,22 @@ self.addEventListener('activate', e => {
 
     e.waitUntil(respuesta);
 
+});
+
+self.addEventListener('fetch', e => {
+
+    const respuesta = caches.match(e.request).then(res => {
+
+        if (res) {
+            return res;
+        } else {
+            return fetch(e.request).then(newRes => {
+
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+
+            });
+        }
+    });
+
+    e.respondWith(respuesta);
 });
